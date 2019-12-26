794ba65fdd0a28b7262ed80714e4f90d31f112c5
Abdul Basit <abdul_basit313@outlook.com>
2019-12-27 01:56:28 +0500

    create, read and delete todo

---
+++ src/components/AddForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="don't forget what has to be done" />
+    <button>Add</button>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: "AddForm",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: Math.random(),
+        title: this.title,
+        completed: false
+      };
+      // send to parent
+      if (!this.title.trim()) {
+        alert("Input can't be empty");
+      } else {
+        this.$emit("add-todo", newTodo);
+      }
+      this.title = "";
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed" />
+      {{todo.title}}
+      <button>Edit</button>
+      <button @click="$emit('del-todo', todo.id)">Delete</button>
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.is-complete {
+  text-decoration: line-through;
+}
+</style>
